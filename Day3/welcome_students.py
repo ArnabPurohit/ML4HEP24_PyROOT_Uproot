f1d5f48baa6c0ca45fdc821428ba091d87f41f42
ARNAB PUROHIT <arnab.purohit@cern.ch>
2024-06-13 11:05:51 +0200

    Create welcome_students.py

---
+++ Day3/welcome_students.py
@@ -0,0 +1,19 @@
+# python3 welcome_students.py
+
+def welcome_students():
+    welcome_message = """
+    ===================================
+    Welcome to Day3 of the ML4HEP PyROOT and Uproot Lecture cum Tutorial!
+    ===================================
+    
+    I'm excited to have you here. Today, we'll explore how the fundamental Physics can be explored using Python, and C++.
+    Let's dive into the fascinating world of fundamental science with the help of computer technology together!
+    
+    If you have any questions, feel free to ask at any time.
+    
+    Happy Learning!
+    """
+    print(welcome_message)
+
+if __name__ == "__main__":
+    welcome_students()
